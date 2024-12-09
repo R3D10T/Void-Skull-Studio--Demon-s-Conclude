01d9a0d38a8edddfdacbd50be7ef74596abcba71
Ashley Jorge <159258668+ImBoxeq@users.noreply.github.com>
2024-12-09 14:20:13 +0000

    Scene Transition
    Can now drag and drop a unity scene to a story scene data.
    
    Edited Game Controller
    Edited Story Scene
    Added Intro to scene build settings

--- Assets/Novel Intro/Scripts/Controllers/GameController.cs
+++ Assets/Novel Intro/Scripts/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -16,21 +17,41 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             if (bottomBar.IsCompleted())
             {
                 if (bottomBar.IsLastSentence())
                 {
-                    currentScene = currentScene.nextScene;
-                    bottomBar.PlayScene(currentScene);
-                    backgroundController.SwitchImage(currentScene.background);
+                    HandleSceneTransition();
                 }
                 else
                 {
                     bottomBar.PlayNextSentence();
                 }
             }
+        }
+    }
+
+    void HandleSceneTransition()
+    {
+        string unitySceneName = currentScene.UnitySceneName;
+
+        if (!string.IsNullOrEmpty(unitySceneName))
+        {
+            // Load the Unity scene specified in the current StoryScene
+            SceneManager.LoadScene(unitySceneName);
+        }
+        else if (currentScene.nextScene != null)
+        {
+            // Otherwise, proceed to the next in-game StoryScene
+            currentScene = currentScene.nextScene;
+            bottomBar.PlayScene(currentScene);
+            backgroundController.SwitchImage(currentScene.background);
+        }
+        else
+        {
+            Debug.LogWarning("No next scene or Unity scene specified!");
         }
     }
 }
--- Assets/Novel Intro/Scripts/Entities/StoryScene.cs
+++ Assets/Novel Intro/Scripts/Entities/StoryScene.cs
@@ -1,13 +1,27 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-[CreateAssetMenu(fileName = "NewStoryScene", menuName ="Data/New Story Scene")]
+[CreateAssetMenu(fileName = "NewStoryScene", menuName = "Data/New Story Scene")]
 [System.Serializable]
 public class StoryScene : ScriptableObject
 {
     public List<Sentence> sentences;
     public Sprite background;
     public StoryScene nextScene;
+
+    [SerializeField]
+    private SceneAsset nextUnityScene;
+
+    public string UnitySceneName
+    {
+
+        get
+        {
+            // Return the name of the scene asset if it's set
+            return nextUnityScene != null ? nextUnityScene.name : string.Empty;
+        }
+    }
 
     [System.Serializable]
     public struct Sentence
