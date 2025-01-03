bfe7404033ad39944ef0de092d09c361468c8fae
Ashley Jorge <159258668+ImBoxeq@users.noreply.github.com>
2025-01-03 16:22:38 +0000

    Added Parallax Effect
    Added backgrounds files
    Created 2 scripts (Parallax , cameraMove)
    Edited Main Camera

---
+++ Assets/Backgrounds/Parallax BG/Parallax.cs
@@ -0,0 +1,38 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+public class Parallax : MonoBehaviour
+{
+
+    private float length;
+    private float StartPos;
+    public GameObject Camera;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (Camera.transform.position.x * (1 - speed));
+        float distance = (Camera.transform.position.x * speed);
+
+        transform.position = new Vector3(StartPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > StartPos + length)
+        {
+            StartPos += length;
+
+        }
+        else if (temp < StartPos - length)
+            StartPos = length;
+
+    }
+}
