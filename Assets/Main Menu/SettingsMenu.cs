9b6cdb97c4b739d5a0575637b8004036a7634bfe
Ashley Jorge <159258668+ImBoxeq@users.noreply.github.com>
2025-03-18 17:42:40 +0000

    Settings Upgrade

--- Assets/Main Menu/SettingsMenu.cs
+++ Assets/Main Menu/SettingsMenu.cs
@@ -10,4 +10,9 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMixer.SetFloat("Volume", volume);
     }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 }
